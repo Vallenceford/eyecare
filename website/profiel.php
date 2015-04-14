d17e6ae2a1015f55f5d5569b7bdf502fc4513764
Noedel-Man <12781@ma-web.nl>
2015-04-14 19:38:12 +0200

    profiel pagina update

---
+++ website/profiel.php
@@ -0,0 +1,26 @@
+<?php include 'php/header.php'; ?>
+    <link rel="stylesheet" href="style/profiel.css" type="text/css" />
+
+<section>
+    <div>
+        <img src="http://devimg.com/100x100">
+        <a href="#">wijzig profiel</a>
+    </div>
+    <section>
+        <h2>Noeël Moeskops</h2>
+        <b>geregistreerd</b>
+        <p>vandaag</p>
+        <b>Laatste bericht</b>
+        <p>gister</p>
+        <b>aantal berichten</b>
+        <p>over 9000</p>
+    </section>
+
+
+
+</section>
+
+
+
+
+<?php include 'php/footer.php'; ?>
